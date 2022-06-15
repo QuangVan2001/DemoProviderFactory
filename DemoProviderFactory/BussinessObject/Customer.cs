8e0e71d0639e91a34c28ff9bb583c228178785b5
QuangVan2001 <quangvanpham02022001@gmail.com>
2022-06-15 22:10:54 +0700

    Demo providerfactory

---
+++ DemoProviderFactory/BussinessObject/Customer.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Prn.Se1623;
+public class Customer
+{
+    // Properties (not field)
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public Customer() { }
+
+    public Customer(string id, string name, string address, string phone)
+    {
+        Id = id;
+        Name = name;
+        Address = address;
+        Phone = phone;
+    }
+
+
+
+    public override string? ToString()
+    {
+        return $"[Id = {this.Id}, Name = {this.Name}, Adress = {this.Address}, PHone = {this.Phone}]";
+    }
+
+
+}
+
---
+++ DemoProviderFactory/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+
+namespace Prn.Se1623;
+public class Program
+{
+    public static void Main()
+    {
+        // test 
+        //Console.WriteLine(GetConnectionString());
+        //Console.ReadLine();
+
+        //GetAllCustomer();
+        //Console.ReadLine();
+
+        List<Customer> customers =  GetAllCustomer();
+        foreach(Customer customer in customers)
+            Console.WriteLine(customer);
+
+    }
+
+    private static string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("AppSetting.json")
+            .Build();
+        return config["ConnectionStrings:FptEduDBConn"];
+    }
+
+    public static List<Customer> GetAllCustomer()
+    {
+        //0. vsql
+        string vSQL = "select * from Customers";       
+        List<Customer> list = new List<Customer>();
+        //1 create a connection to SQL Server
+        DbProviderFactory factory = SqlClientFactory.Instance;
+        DbConnection? connection = factory.CreateConnection();//  dau hoi cham (?) cho 
+        if(connection == null)
+        {
+            Console.WriteLine($"Init Connection fail .....");
+            return null;
+        }
+        connection.ConnectionString = GetConnectionString();
+        connection.Open();
+        //2. initial command (sql) to query data
+        DbCommand? command = factory.CreateCommand();
+        command.CommandText = vSQL;
+        command.Connection = connection;
+
+        //3. excute command
+        DbDataReader dataReader = command.ExecuteReader();// nổ máy
+        //4. read data and show to console
+        while (dataReader.Read())
+        {
+            Customer customer = new Customer();
+            customer.Id = (string)dataReader["CustomerId"];
+            customer.Name = (string)dataReader["CompanyName"];
+            customer.Address = (string)dataReader["Address"];
+            customer.Phone = (string)dataReader["Phone"];
+            list.Add(customer); 
+        }
+        return list;
+    }
+}
